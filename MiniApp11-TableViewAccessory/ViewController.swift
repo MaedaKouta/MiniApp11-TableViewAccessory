34e5664097095f497bac92c0211012148f6f7630
MaedaKouta <maedakoutaa@gmail.com>
2022-02-20 08:18:33 +0900

    commit

--- MiniApp11-TableViewAccessory/ViewController.swift
+++ MiniApp11-TableViewAccessory/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textLabel.text = "未選択"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row + 1)行目"
+        return cell
+
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        textLabel.text = "\(indexPath.row + 1)行目がタッチ"
+    }
+
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        textLabel.text = "\(indexPath.row + 1)行目の「i」がタッチ"
+    }
 
 }
 
